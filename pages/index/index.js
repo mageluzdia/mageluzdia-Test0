0685c44ea04a804b25854c2604f9235cea863faa
mageluzdia <309371157@qq.com>
2020-04-08 22:55:46 +0800

    Mis: Test modify on mac

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'World is bad!',
+    motto: 'World is not bad!',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
